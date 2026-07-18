2680ae42768e233abab3bb7ec02070633c13c8f1
Trevor Grabham <trevarus1997@gmail.com>
2026-07-17 20:43:46 -0700

    completed day 2, part 1

--- day-1/part-1/main.go
+++ day-1/part-1/main.go
@@ -1,16 +1,51 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	inputData, err := os.ReadFile("input/example.txt")
+	inputData, err := os.ReadFile("input/challenge.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
+	inputData = bytes.TrimSpace(inputData)
 
-	fmt.Println(string(inputData))
+	var numberOfZeros int
+	dialPosition := 50
+
+	inputLines := strings.SplitSeq(string(inputData), "\n")
+
+	// for each input line
+	for inputLine := range inputLines {
+		//		grab the direction
+		direction := inputLine[0]
+
+		//		grab the number of rotations
+		var numberRotations int
+		numberRotations, err = strconv.Atoi(inputLine[1:])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		//		update dialPosition and check if dialPosition == 0
+		switch direction {
+		case 'L':
+			dialPosition = (dialPosition - numberRotations) % 100
+		case 'R':
+			dialPosition = (dialPosition + numberRotations) % 100
+		default:
+			log.Fatalf("unknown direction %s", string(direction))
+		}
+		if dialPosition == 0 {
+			numberOfZeros++
+		}
+	}
+
+	fmt.Println(numberOfZeros)
 }
